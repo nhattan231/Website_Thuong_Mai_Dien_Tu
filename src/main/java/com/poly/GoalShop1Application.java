0861f204c78a522e77da48194630980d8f65079f
nhattan231 <88882616+nhattan231@users.noreply.github.com>
2022-02-28 20:27:44 +0700

    GoalShop

---
+++ src/main/java/com/poly/GoalShop1Application.java
@@ -0,0 +1,14 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@SpringBootApplication
+public class GoalShop1Application {
+	// cái này là chạy Application
+	public static void main(String[] args) {
+		SpringApplication.run(GoalShop1Application.class, args);
+	}
+	
+}
